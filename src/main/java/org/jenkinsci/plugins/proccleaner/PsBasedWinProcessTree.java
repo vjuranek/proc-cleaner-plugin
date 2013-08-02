a914b0fc6de8282d870e217abe84e7e3a8ddf1ff
psrna <psrna@redhat.com>
2013-08-02 14:18:06 +0200

    Refactoring for upcoming platform independent solution.
    * Introduces new factory mathods for PsBasedProcessTree and PsProcess.
    * PsBasedProcessTree class is abstract
    * PsProcess class is abstract and extracted from PsBasedProcessTree class (was nested before)
    * NEW Prototype classes for windows
    * NEW classes for unix specific implementation
    * NEW PsProcessWin and PsProcessUnix because of kill method specifics on different platform.

---
+++ src/main/java/org/jenkinsci/plugins/proccleaner/PsBasedWinProcessTree.java
@@ -0,0 +1,17 @@
+package org.jenkinsci.plugins.proccleaner;
+
+import java.io.IOException;
+
+/**
+ * Author: psrna
+ * Date: 8/2/13
+ */
+public class PsBasedWinProcessTree extends PsBasedProcessTree {
+
+    @Override
+    public PsBasedProcessTree createProcessTreeFor(String user) throws InterruptedException, IOException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+
+}
